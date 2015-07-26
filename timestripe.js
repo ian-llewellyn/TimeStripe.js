2bc698c3186a1f7b3f11c31912c785487dc83c22
Ian Llewellyn <ian_llewellyn@hotmail.com>
2015-07-26 13:34:27 +0100

    Big JS code changes. Moved CSS specfic to the TimeStripe out of the CSS file - will abandon CSS file soon. Simplified how the library is used - README to follow soon, but in a nutshell:  new TimeStripe(document.getElementById('div-id'))

--- timestripe.js
+++ timestripe.js
@@ -1,4 +1,85 @@
-function updateBar() {
+function TimeStripe(dom_element) {
+  this.element = dom_element;
+  this.actor = 'height';
+  this.pixelsPerHour = 100;
+  this.scrolling = 'yes';
+
+  this.align('left');
+
+  this.style({
+    "width": "100%",
+    "background-color": "rgba(128,0,0,0.3)",
+    "border": "0 0 2px 0",
+    "border-bottom": "solid red",
+    "position": "absolute",
+    "top": 0
+  });
+
+  var ts_obj = this;
+  window.onload = function() { ts_obj.updateBar(); };
+}
+
+TimeStripe.prototype.style = function(styleObject) {
+  styleObject = typeof styleObject !== 'undefined' ? styleObject : null;
+
+  if ( styleObject === null ) {
+    throw Error('TimeStripe cannot yet retrieve styles.');
+  }
+
+  for ( var attribute in styleObject ) {
+    var value = styleObject[attribute];
+    this.element.style.setProperty(attribute, value);
+  }
+}
+
+TimeStripe.prototype.align = function(alignment) {
+  alignment = typeof alignment !== 'undefined' ? alignment : 'left';
+
+  switch ( alignment ) {
+  case 'left':
+    this.animate('width');
+    this.style({"top": 0, "left": 0});
+    break;
+  case 'right':
+    this.animate('width')
+    this.style({"top": 0, "right": 0});
+    break;
+  case 'top':
+    this.animate('height')
+    this.style({"top": 0, "left": 0});
+    break;
+  case 'bottom':
+    this.animate('height')
+    this.style({"bottom": 0, "left": 0});
+    break;
+  }
+}
+
+TimeStripe.prototype.animate = function(property) {
+  if ( typeof property !== 'undefined' ) {
+    this.actor = property;
+  } else {
+    return this.actor;
+  }
+}
+
+TimeStripe.prototype.pixelsPerHour = function(value) {
+  if ( typeof value !== 'undefined' ) {
+    this.pixelsPerHour = value;
+  } else {
+    return this.pixelsPerHour;
+  }
+}
+
+TimeStripe.prototype.scroll = function(value) {
+  if ( typeof value !== 'undefined' ) {
+    this.scrolling = value;
+  } else {
+    return this.scrolling;
+  }
+}
+
+TimeStripe.prototype.updateBar = function() {
   var hour_size = 100; // Include the thickness of top and bottom borders.
 
   // Adjust Bar Postition / Div Size
@@ -8,21 +89,22 @@ function updateBar() {
 
   var height = (hour * hour_size) + ((minute / 60) * hour_size);
 
-  document.getElementById('time-bar').style.height = height;
+  this.element.style.height = height;
 
-  scrollPage();
+  if ( this.scrolling == 'yes' ) this.scrollPage();
 
-  setTimeout(function() { updateBar(); }, 30000);
+  var ts_obj = this;
+  setTimeout(function() { ts_obj.updateBar(); }, 30000);
 }
 
-function scrollPage() {
-  var time_bar_pos = parseInt(document.getElementById('time-bar').style.height);
+TimeStripe.prototype.scrollPage = function() {
+  var time_bar_pos = parseInt(this.element.style.height);
 
   if (time_bar_pos > 80) {
-    window.scrollTo(0, time_bar_pos - 80);
+    while ( window.pageYOffset != (time_bar_pos - 80) ) {
+      window.scrollTo(0, time_bar_pos - 80);
+    }
   } else {
     window.scrollTo(0, 0);
   }
 }
-
-window.onload = function() { setTimeout(updateBar(), 2000); };
